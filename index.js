83fff0cf90eeb0d1f3a5d436b818436da815c1bd
lashatatu <lashatatu@gmail.com>
2020-10-06 18:04:47 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: '550de292',
+            s: searchTerm
+        }
+    });
+
+    console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
+const debounce = (func, delay=1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
+
+let timeoutId;
+const onInput = (event => {
+    fetchData(event.target.value);
+});
+input.addEventListener('input', debounce(onInput, 500));
